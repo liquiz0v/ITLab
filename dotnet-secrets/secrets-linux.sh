ecc216525fd978676972ef0d073f7d9f197746bf
Vladislav <liquiz@ukr.net>
2019-11-06 18:16:13 +0200

    Fixed authentication
    - Added dotnet-secrets folder with secrets of facebook, google and email, required to run on server
    - Fixed database
    - Fixed EmailSender

---
+++ dotnet-secrets/secrets-linux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dotnet user-secrets set "SendGridUser" "ITLab"
+dotnet user-secrets set "SendGridKey" "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"
+dotnet user-secrets set "Authentication:Google:ClientId" "615117915602-e3e9is5f8aq9p4lrn7jr50bmg7o7ujqi.apps.googleusercontent.com"
+dotnet user-secrets set "Authentication:Facebook:AppSecret" "67e89d68aed2bb86edb7433cb44e7008"
+dotnet user-secrets set "Authentication:Facebook:AppId" "329576271250688"
+dotnet user-secrets set "Authentication:Google:ClientSecret" "QqbLs44B6T7e0-Ja8s_OABOs"
