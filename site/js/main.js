7100fe9f61ecd9bdeda046dc5f053870274340a6
Leonid <Leon9539@gmail.com>
2019-11-09 02:25:53 +0200

    Add slider

--- site/js/main.js
+++ site/js/main.js
@@ -35,3 +35,61 @@ let arrowDown = document.getElementById('arrowDown');
 function toElem(elem){
 	elem.scrollIntoView({block: 'start', behavior: 'smooth'});
 }
+
+let slideLeft = document.getElementById('slideLeft');
+let slideRight = document.getElementById('slideRight');
+let sliderWrapper = document.querySelector('.slider-wrapper');
+let counter = 3;
+
+slideRight.onclick = function(){
+	let elem = document.querySelectorAll('.slider-news');
+	if(counter < elem.length){
+		sliderWrapper.style.left = sliderWrapper.offsetLeft - 320 + 'px';
+		counter++;
+		if(counter == elem.length){
+			slideLeft.style.visibility = 'visible';
+			slideRight.style.visibility = 'hidden';
+		}else{
+			slideRight.style.visibility = 'visible';
+		}
+	}else{
+		slideRight.style.visibility = 'hidden';
+	}
+}
+slideLeft.onclick = function(){
+	if(counter > 3){
+		sliderWrapper.style.left = sliderWrapper.offsetLeft + 320 + 'px';
+		counter--;
+		console.log(counter);
+		if(counter == 3){
+			slideLeft.style.visibility = 'hidden';
+			slideRight.style.visibility = 'visible';
+		}else{
+			slideLeft.style.visibility = 'visible';
+		}
+	}else{
+		slideLeft.style.visibility = 'hidden';
+	}
+}
+
+let sendFeedback = document.getElementById('sendFeedback');
+
+sendFeedback.onclick = function(){
+	let xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.onreadyechange = function(){
+		if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+           if (xmlhttp.status == 200) {
+               let d1 = document.querySelector('.fName');
+               let d2 = document.querySelector('.fTel');
+               let d3 = document.querySelector('.fAsk');
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+	}
+}
