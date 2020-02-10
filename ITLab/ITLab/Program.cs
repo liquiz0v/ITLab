afd98f8245690f9cce555b7bea84f9b20635b1ef
Vladislav <liquiz@ukr.net>
2020-02-10 18:49:15 +0200

    Updated host address for docker

--- ITLab/ITLab/Program.cs
+++ ITLab/ITLab/Program.cs
@@ -20,7 +20,8 @@ namespace ITLab
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseUrls("http://*:5000");
                 });
     }
 }
