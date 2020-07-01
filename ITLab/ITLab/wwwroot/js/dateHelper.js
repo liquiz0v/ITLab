c390174937d0bf4b86f025869e53be12d03389e3
artem.vasylenko <vasylenko.w@gmail.com>
2020-07-01 23:35:53 +0300

    changed version of the moment lib to with locale for translate into Russian

--- ITLab/ITLab/wwwroot/js/dateHelper.js
+++ ITLab/ITLab/wwwroot/js/dateHelper.js
@@ -1,10 +1,11 @@
 const dateFormat = 'DD-MMM-YYYY';
 const dateTimeFormat = 'DD-MMM-YYYY HH:mm';
+moment.locale();  
 
 const formatDateString = (dateToFormat, withTime) => {
     if (!dateToFormat || dateToFormat.trim().length === 0) {
         return dateToFormat;
     }
-    return moment(new Date(dateToFormat)).format(withTime ? dateTimeFormat : dateFormat);
+    return moment(new Date(dateToFormat)).lang("ru").format(withTime ? dateTimeFormat : dateFormat);
 };
 
