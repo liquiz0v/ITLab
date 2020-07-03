0661ab4d0e86f45cc19de7837d7b3fb8e69d98ff
Artem Vasylenko <artem.vasylenko@sigma.software>
2020-07-04 00:12:15 +0300

    connect Redux and implement basic architecture for Redux and fetch api, create helpers for connect to api.

--- WebApps/ITLab.Cabinet/app/src/index.tsx
+++ WebApps/ITLab.Cabinet/app/src/index.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import rootReducer from './reducer'
 import * as serviceWorker from './serviceWorker';
 
+
+const store = createStore(rootReducer)
+
 ReactDOM.render(
-  <React.StrictMode>
+
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
+
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
