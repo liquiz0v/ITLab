5897cdf221d0163ac1362c05541344f836351ffc
artem.vasylenko <vasylenko.w@gmail.com>
2020-07-06 23:15:07 +0300

    added MainLayout, StudentDetails,antd, react-router
    added main layout component, student details component, antd lib, react router dom lib,
    need to choose MainLayout template from antd, and configure routes

--- WebApps/ITLab.Cabinet/app/src/App.tsx
+++ WebApps/ITLab.Cabinet/app/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MainLayout from './components/Layout/MainLayout/MainLayout'
+import StudentDetails from 'pages/Student/StudentDetails/StudentDetails';
+
 
 function App() {
   
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter> 
+      <Switch>
+
+          <Route path="/student-details" exact={true} component={StudentDetails} />  
+
+      </Switch>
+      <MainLayout/>
+    </BrowserRouter>
   );
 }
 
