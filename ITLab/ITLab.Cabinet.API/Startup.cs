bee04a6982cc85fa1ae5ccf9bcad76b318f8a4ed
vasylenkoArtem <44374504+vasylenkoArtem@users.noreply.github.com>
2020-07-10 21:34:14 +0300

    Configure di for cabinet (#11)
    * config autofac неуспешноаааааа
    
    * Fixed config Autofac
    
    TODO: Use config in other projects.
    
    * DI WORKS!
    
    HOORAY!!!!
    
    * a bit of refactor to DI configuring
    
    * Changed Startup for CD pipeline
    
    Co-authored-by: Artem Vasylenko <artem.vasylenko@sigma.software>
    Co-authored-by: Vladislav Sheyko <liquiz@ukr.net>

--- ITLab/ITLab.Cabinet.API/Startup.cs
+++ ITLab/ITLab.Cabinet.API/Startup.cs
@@ -2,43 +2,66 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using ITLab.Cabinet.API.configs;
+using ITLab.Cabinet.Logic.Services;
+using ITLab.Cabinet.Logic.Services.Interfaces;
+//using Autofac;
+//using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using ITLab.Cabinet.Database.Models;
-using Microsoft.AspNetCore.HttpOverrides;
 
 namespace ITLab.Cabinet.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            // In ASP.NET Core 3.0 `env` will be an IWebHostEnvironment, not IHostingEnvironment.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            this.Configuration = builder.Build();
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
+        public ILifetimeScope AutofacContainer { get; private set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CabinetContext>(i => i.UseSqlServer(Configuration.GetConnectionString("CabinetConnection"), b => b.MigrationsAssembly("ITLab.Cabinet.Database")));
             services.AddControllers();
+            services.AddAutofac();
         }
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
 
+            // Register your own things directly with Autofac, like:
+            builder.RegisterModule(new ITLabCabinetAutofacConfig());
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            this.AutofacContainer = app.ApplicationServices.GetAutofacRoot();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
 
             //app.UseHttpsRedirection();
 
@@ -53,9 +76,7 @@ namespace ITLab.Cabinet.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}");
+                endpoints.MapControllers();
             });
         }
     }
