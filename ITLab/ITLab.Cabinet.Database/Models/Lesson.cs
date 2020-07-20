738ae4988ddbedc1fb1317f6ea8d538c96aaa841
Vladislav Sheyko <liquiz@ukr.net>
2020-07-20 14:53:37 +0300

    Fixed couple links, added models

--- ITLab/ITLab.Cabinet.Database/Models/Lesson.cs
+++ ITLab/ITLab.Cabinet.Database/Models/Lesson.cs
@@ -11,6 +11,7 @@ namespace ITLab.Cabinet.Database.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public Course Course { get; set; }
+        public DateTime LessonDate { get; set; }
         public DateTime LessonDateFrom { get; set; }
         public DateTime LessonDateTo { get; set; }
         public ICollection<Presentations> Presentations { get; set; }
