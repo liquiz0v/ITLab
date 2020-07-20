c5cf122918643bfc46123f8a91492fc0eefd3b8b
Vladislav Sheyko <liquiz@ukr.net>
2020-07-20 13:26:44 +0300

    Moved css stylings to React
    TODO: Optimize stylings && create new components

--- WebApps/ITLab.Cabinet/app/src/components/Layout/MainLayout/MainLayout.tsx
+++ WebApps/ITLab.Cabinet/app/src/components/Layout/MainLayout/MainLayout.tsx
@@ -4,6 +4,7 @@ import { Menu } from 'antd'
 import { Link } from 'react-router-dom';
 import  StudentDetails from "../../../pages/Student/StudentDetails/StudentDetails";
 
+import '../../../App.css';
 
 
 class MainLayout extends React.Component<any> {
@@ -17,23 +18,53 @@ class MainLayout extends React.Component<any> {
     }
 
     render() {
-        return (
-             <Menu>
-             <Menu.Item key="/StudentDetails">
-                <span>
-                    <Link to="/student-details">
-                        <StudentDetails></StudentDetails>
-                    </Link>
-                </span>
-             </Menu.Item>
-            <Menu.Item key="/StudentStatistics">
-                <span>
-                    <Link to="/StudentStatistics">
-                        <div>StudentStatistics</div>
-                    </Link>
-                </span>
-             </Menu.Item>
+        const navBar : JSX.Element = (
+            <div>
+                <header>
+                    <div className="Header-wrapper">
+                        <nav>
+                            <ul>
+                                <li><a href="/"><span>IT Lab Odessa</span></a></li>
+                                <li><a href="">Профиль</a></li>
+                            </ul>
+                        </nav>
+                        <div className="Header-profile">
+                            
+                            <div className="Avatar">
+                                <img src="images/avatar.png"></img>
+                            </div>
+                            <ul>
+                                <li><a href="#">Редактирование профиля</a></li>
+                                <li><a href="#">Выход</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        );
+        
+        const siderMenu : JSX.Element = (
+            <Menu
+                style={{ width: 256 }}
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+            >
+                <Menu.Item key="/StudentDetails">
+                    <span>
+                        <Link to="/student-details">
+                            <StudentDetails></StudentDetails>
+                        </Link>
+                    </span>
+                </Menu.Item>
              </Menu>
+        );
+
+        return (
+            <>
+                {navBar}
+                {siderMenu}
+            </>
         );
     }
 }
