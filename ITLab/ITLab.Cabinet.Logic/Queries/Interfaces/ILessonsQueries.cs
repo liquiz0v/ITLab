74257584adaf7e8fd92949fec0256cfca0c20b7d
vasylenkoArtem <44374504+vasylenkoArtem@users.noreply.github.com>
2020-08-14 00:58:03 +0300

    added get lesson info list by course id and student id (#29)
    Co-authored-by: Artem Vasylenko <artem.vasylenko@sigma.software>

---
+++ ITLab/ITLab.Cabinet.Logic/Queries/Interfaces/ILessonsQueries.cs
@@ -0,0 +1,13 @@
+﻿using ITLab.Cabinet.Logic.DTOModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITLab.Cabinet.Logic.Queries.Interfaces
+{
+    public interface ILessonsQueries
+    {
+        Task<IEnumerable<LessonDTO>> GetLessonsAsync(int courseId, int studentId);
+    }
+}
