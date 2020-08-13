b593c60140cc9e77888bf95de1e5ba9e61e651ef
vasylenkoArtem <44374504+vasylenkoArtem@users.noreply.github.com>
2020-08-13 23:45:03 +0300

    feature/StudentCoursesStatistics-Add visited lessons to student statistics (#28)
    * added visited Lessons to student statistics
    
    * fix namings
    
    Co-authored-by: Artem Vasylenko <artem.vasylenko@sigma.software>

---
+++ ITLab/ITLab.Cabinet.Database/Migrations/20200813193356_AddedLessonsVisitsTableAndConnections.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ITLab.Cabinet.Database.Migrations
+{
+    public partial class AddedLessonsVisitsTableAndConnections : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LessonsVisits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(nullable: true),
+                    LessonId = table.Column<int>(nullable: true),
+                    Visited = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LessonsVisits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LessonsVisits_Lessons_LessonId",
+                        column: x => x.LessonId,
+                        principalTable: "Lessons",
+                        principalColumn: "LessonId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LessonsVisits_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LessonsVisits_LessonId",
+                table: "LessonsVisits",
+                column: "LessonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LessonsVisits_StudentId",
+                table: "LessonsVisits",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LessonsVisits");
+        }
+    }
+}
--- ITLab/ITLab.Cabinet.Logic/DTOModels/StudentStatisticsDTO.cs
+++ ITLab/ITLab.Cabinet.Logic/DTOModels/StudentStatisticsDTO.cs
@@ -7,5 +7,7 @@
         public int AverageMark { get; set; }
         public int CompletedTasksCount { get; set; }
         public int OverallTasksCount { get; set; }
+        public int CompletedLessons { get; set; }
+        public int  VisitedLessons { get; set; }
     }
 }
