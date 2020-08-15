5f53bcc5990201792f49ff9b5f9e3644addfccbc
Vladislav Sheyko <liquiz@ukr.net>
2020-08-15 17:44:56 +0300

    Added data queries (records)

---
+++ db_queries/ITLab/StudentMarksRecords.sql
@@ -0,0 +1,140 @@
+USE [ITLab_Cabinet]
+GO
+
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (2
+           ,2
+           ,84)
+GO
+
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (3
+           ,1
+           ,58)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (3
+           ,2
+           ,63)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (4
+           ,1
+           ,100)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (4
+           ,2
+           ,65)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (5
+           ,1
+           ,75)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (5
+           ,2
+           ,74)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (6
+           ,1
+           ,54)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (6
+           ,2
+           ,42)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (2
+           ,3
+           ,74)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (2
+           ,4
+           ,12)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (2
+           ,5
+           ,76)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (2
+           ,7
+           ,100)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (2
+           ,7
+           ,45)
+GO
+INSERT INTO [dbo].[StudentMarks]
+           ([StudentId]
+           ,[HomeTaskId]
+           ,[Mark])
+     VALUES
+           (3
+           ,4
+           ,100)
+GO
+
