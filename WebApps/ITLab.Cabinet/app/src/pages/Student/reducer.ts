ce520fbf2e3d6a8aecc68e390bcfae386579f12f
Vladislav Sheyko <45404995+liquiz0v@users.noreply.github.com>
2020-08-15 19:21:50 +0300

    Implemented student courses statistics on Frontend (#30)
    * added ui
    
    * try to fix counter
    
    * added ui
    
    * try to fix counter
    
    * Added data queries (records)
    
    * Implemented Student Statistics (Frontend)
    
    Co-authored-by: Artem Vasylenko <artem.vasylenko@sigma.software>

--- WebApps/ITLab.Cabinet/app/src/pages/Student/reducer.ts
+++ WebApps/ITLab.Cabinet/app/src/pages/Student/reducer.ts
@@ -2,6 +2,7 @@ import { updateObject } from '../../helpers/updateObjectHelper'
 import { GET_STUDENT_INFORMATION } from './StudentDetails/actions'
 import { GET_STUDENT_COURSES } from './StudentDetails/actions'
 import { GET_COURSE_LESSONS } from './StudentDetails/actions'
+import { GET_STUDENT_STATISTICS } from './StudentDetails/actions'
 
 export type Student = {
     StudentId: number;
@@ -25,16 +26,28 @@ export type Course = {
     Lessons: Lesson[];
 };
 
+export type StudentStatistics = {
+    StudentId: number;
+    StudentPositionInRating: number;
+    AverageMark: number;
+    CompletedTasksCount: number;
+    OverallTasksCount: number;
+    CompletedLessons: number;
+    VisitedLessons: number;
+}
+
 export type StudentState = {
     student?: Student,
     studentCourses?: Course[],
-    courseLessons?: Lesson[]
+    courseLessons?: Lesson[],
+    studentStatistics?: StudentStatistics
 };
 
 const initialState: StudentState = {
     student: undefined,
     studentCourses: [],
     courseLessons: [],
+    studentStatistics: undefined
 }
 
 export const reducer = (state: StudentState = initialState, action: any) => {
@@ -51,6 +64,10 @@ export const reducer = (state: StudentState = initialState, action: any) => {
             return updateObject(state, { courseLessons: undefined });
         case GET_COURSE_LESSONS.SUCCESS:
             return updateObject(state, { courseLessons: action.courseLessons });
+        case GET_STUDENT_STATISTICS.REQUEST:
+            return updateObject(state, { studentStatistics: undefined});
+        case GET_STUDENT_STATISTICS.SUCCESS:
+            return updateObject(state, { studentStatistics: action.studentStatistics});
 
         default:
             return state;
